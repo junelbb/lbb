29f496054685fc06b2f48eb425631a404c599b32
李冰冰 <junelbb@163.com>
2019-07-18 10:08:33 +0800

    new

---
+++ beijing-cms/src/main/java/com/lbb/cms/dao/CommentMapper.java
@@ -0,0 +1,9 @@
+package com.lbb.cms.dao;
+
+import com.lbb.cms.domain.Comment;
+
+public interface CommentMapper {
+
+	void save(Comment comment);
+
+}
---
+++ beijing-cms/src/main/java/com/lbb/cms/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.lbb.cms.service;
+
+import com.lbb.cms.domain.Comment;
+
+public interface CommentService {
+
+	void save(Comment comment);
+
+}
