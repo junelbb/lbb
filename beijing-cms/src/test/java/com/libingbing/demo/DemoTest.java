97058844281761503566503a6b2319087176bd64
李冰冰 <junelbb@163.com>
2019-07-30 09:25:53 +0800

    改变测试类

--- beijing-cms/src/test/java/com/libingbing/demo/DemoTest.java
+++ beijing-cms/src/test/java/com/libingbing/demo/DemoTest.java
@@ -9,46 +9,28 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.libingbing.cms.domain.Article;
-import com.libingbing.cms.domain.SpcialArticle;
-import com.libingbing.cms.service.adminService.AdminService;
+import com.libingbing.cms.service.ArticleService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:spring-context.xml")
+@ContextConfiguration("classpath:spring-context.xml")
 
 public class DemoTest {
 	
 	@Autowired
-	AdminService service;
+	ArticleService service;
 
+	
 	@Test
 	public void test1(){
-		SpcialArticle specialById = service.getSpecialById(1);
-		List<Article> articleList = specialById.getArticleList();
-		for (Article article : articleList) {
-			System.out.println(article.getTitle());
-/*			Date created = article.getCreated();
-			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd HH:MM:ss");
-			String format = simpleDateFormat.format(created);
-			System.out.println(format);*/
-		}
+		System.out.println(1);
 	}
 	
 	@Test
 	public void test2(){
-		List<SpcialArticle> selectSpecial = service.selectSpecial();
-		for (SpcialArticle spcialArticle : selectSpecial) {
-			System.out.println(spcialArticle.getTitle());
-		}
-	}
-	
-	@Test
-	public void test3(){
-		SpcialArticle selectSpecialById = service.selectSpecialById(2);
-		List<Article> articleList = selectSpecialById.getArticleList();
-		for (Article article : articleList) {
+		List<Article> queryAll = service.queryAll(new Article());
+		for (Article article : queryAll) {
 			System.out.println(article.getTitle());
 		}
 	}
-	
 	
 }
